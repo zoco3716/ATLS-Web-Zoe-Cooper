05d591d604d5c6f7ae1fec2a3d3f6894c32486ad
zoco3716 <zoco3716@colorado.edu>
2025-04-24 12:56:51 -0600

    js final

--- final/final.js
+++ final/final.js
@@ -4,42 +4,44 @@ var volume = 0;
 var goingUp = true;
 
 document.addEventListener('keydown', function(e) {
-  var left = basket.offsetLeft;
-  if (e.key === 'ArrowLeft') basket.style.left = (left - 20) + 'px';
-  if (e.key === 'ArrowRight') basket.style.left = (left + 20) + 'px';
+    var left = basket.offsetLeft;
+    if (e.key === 'ArrowLeft')
+        basket.style.left = (left - 20) + 'px';
+    if (e.key === 'ArrowRight') 
+        basket.style.left = (left + 20) + 'px';
 });
 
 function dropItem() {
-  var item = document.createElement('div');
-  item.className = 'item';
-  item.style.left = Math.random() * (window.innerWidth - 30) + 'px';
-  item.style.top = '0px';
-  document.body.appendChild(item);
+    var item = document.createElement('div');
+    item.className = 'item';
+    item.style.left = Math.random() * (window.innerWidth - 30) + 'px';
+    item.style.top = '0px';
+    document.body.appendChild(item);
 
-  var fall = setInterval(function() {
-    item.style.top = (item.offsetTop + 5) + 'px';
+    var fall = setInterval(function() {
+        item.style.top = (item.offsetTop + 5) + 'px';
 
-    var basketLeft = basket.offsetLeft;
-    var itemLeft = item.offsetLeft;
+        var basketLeft = basket.offsetLeft;
+        var itemLeft = item.offsetLeft;
 
-    if (item.offsetTop > window.innerHeight - 60 &&
-        itemLeft > basketLeft - 30 && itemLeft < basketLeft + 80) {
-    
+        if (item.offsetTop > window.innerHeight - 60 && itemLeft > basketLeft - 100 && itemLeft < basketLeft + 80) {
 
-    if (goingUp) {
-        volume++;
-        if (volume >= 10) goingUp = false;
-      } else {
-        volume--;
-        if (volume <= 0) goingUp = true;
-      }
+            if (goingUp) {
+                volume++;
+                if (volume >= 10) 
+                    goingUp = false;
+                } 
+                else {
+                    volume--;
+                    if (volume <= 0) goingUp = true;
+            }
 
-      scoreDisplay.textContent = 'Volume: ' + volume;
-      item.remove();
-      clearInterval(fall);
-    }
+            scoreDisplay.textContent = 'VOLUME: ' + volume;
+            item.remove();
+            clearInterval(fall);
+        }
 
-  }, 20);
+    }, 20);
 }
 
 setInterval(dropItem, 1500);
