8e2625213f892719a05df3b11f79aa938fdfdefa
zoco3716 <zoco3716@colorado.edu>
2025-04-27 12:36:48 -0600

    final updates

--- final/final.js
+++ final/final.js
@@ -1,46 +1,54 @@
 var basket = document.getElementById('basket');
-var scoreDisplay = document.getElementById('volume');
-var volume = 0;
-var goingUp = true;
+var currentDigitDisplay = document.getElementById('currentDigit');
+var phoneDisplay = document.getElementById('phoneDisplay');
+var currentDigit = 0;
+var phoneNumber = '';
+var starCount = 0;
 
 document.addEventListener('keydown', function(e) {
     var left = basket.offsetLeft;
-    if (e.key === 'ArrowLeft')
+    if (e.key === 'ArrowLeft') {
         basket.style.left = (left - 20) + 'px';
-    if (e.key === 'ArrowRight') 
+    } else if (e.key === 'ArrowRight') {
         basket.style.left = (left + 20) + 'px';
+    } else if (e.code === 'Space') {
+        phoneNumber += currentDigit;
+        phoneDisplay.textContent = 'PHONE: ' + phoneNumber;
+        currentDigit = 0;
+        currentDigitDisplay.textContent = 'CURRENT DIGIT: ' + currentDigit;
+    }
 });
 
 function dropItem() {
+    if (phoneNumber.length >= 10) return;
+
     var item = document.createElement('div');
     item.className = 'item';
     item.style.left = Math.random() * (window.innerWidth - 30) + 'px';
     item.style.top = '0px';
     document.body.appendChild(item);
 
+    starCount++;
+
+    if (starCount % 10 === 0) {
+        item.style.backgroundImage = 'url("https://i.ebayimg.com/images/g/VXsAAOSwBnNlop5N/s-l1200.jpg")';
+        item.style.backgroundSize = 'cover';
+    }
+
     var fall = setInterval(function() {
         item.style.top = (item.offsetTop + 5) + 'px';
 
-        var basketLeft = basket.offsetLeft;
-        var itemLeft = item.offsetLeft;
+        if (item.offsetTop > window.innerHeight - 60) {
+            var basketLeft = basket.offsetLeft;
+            var itemLeft = item.offsetLeft;
 
-        if (item.offsetTop > window.innerHeight - 60 && itemLeft > basketLeft - 100 && itemLeft < basketLeft + 80) {
-
-            if (goingUp) {
-                volume++;
-                if (volume >= 10) 
-                    goingUp = false;
-                } 
-                else {
-                    volume--;
-                    if (volume <= 0) goingUp = true;
+            if (itemLeft > basketLeft - 100 && itemLeft < basketLeft + 80) {
+                currentDigit = (currentDigit + 1) % 10;
+                currentDigitDisplay.textContent = 'CURRENT DIGIT: ' + currentDigit;
+                item.remove();
+                clearInterval(fall);
             }
-
-            scoreDisplay.textContent = 'VOLUME: ' + volume;
-            item.remove();
-            clearInterval(fall);
         }
-
     }, 20);
 }
 
